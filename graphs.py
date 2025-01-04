e696185669049fe1307b0955ca8cb2dec1594da8
Alec.Schmidt <alecschmidt2013@gmail.com>
2025-01-04 13:56:36 +0100

    changed python script for graph labels

--- graphs.py
+++ graphs.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import json
 
+x_labels = ["2", "4", "8", "16", "32", "64", "128", "256"]
+y_label = "Execution time (s)"
 
 if __name__ == "__main__":
     with open('export.json', 'r') as file:
@@ -8,12 +10,12 @@ if __name__ == "__main__":
     
     x = []
     heights = []
-    for result in data["results"]:
-        x.append(result["command"])
+    for i, result in enumerate(data["results"]):
+        x.append(x_labels[i])
         heights.append(result["mean"])
 
     plt.title("Execution time by thread count")
     plt.xlabel("Thread count")
-    plt.ylabel("Execution time (ms)")
+    plt.ylabel(y_label)
     plt.bar(x, heights)
     plt.show()
