0c6d1c9354659f73fe5bb0a96f2d7ac1adad80e3
Loïc Fontaine <92462929+loicfontaine@users.noreply.github.com>
2025-01-05 20:48:52 +0100

    Update do-it.sh

--- do-it.sh
+++ do-it.sh
@@ -5,8 +5,12 @@ FILENAME=dj38.tsp
 
 hyperfine "./tspcc -f $FILENAME -t 2" \
             "./tspcc -f $FILENAME -t 4" \
-            "./tspcc -f $FILENAME -t 6" \
             "./tspcc -f $FILENAME -t 8" \
+            "./tspcc -f $FILENAME -t 16" \
+            "./tspcc -f $FILENAME -t 32" \
+            "./tspcc -f $FILENAME -t 64" \
+            "./tspcc -f $FILENAME -t 128" \
+            "./tspcc -f $FILENAME -t 256" \
             --export-json ./export.json
 
 python3 ./graphs.py
